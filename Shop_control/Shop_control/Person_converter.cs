bbc16c64b852752966a42d69d5594c0a59c44046
Elay <ovramecilla@gmail.com>
2022-10-09 19:33:02 +0300

    Change Person.html . Now the radio button work.

--- Shop_control/Shop_control/Person_converter.cs
+++ Shop_control/Shop_control/Person_converter.cs
@@ -1,13 +1,14 @@
 ﻿using System.Text.Json.Serialization;
 using System.Text.Json;
 using Shop_control.Data_base;
+using System;
 
 namespace Shop_control
 {
     public class PersonConverter : JsonConverter<Persons>
     {
         public override Persons Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        { 
+        {
 
             var fname = "Undefined";
             var sname = "Undefined";
@@ -18,29 +19,55 @@ namespace Shop_control
             {
                 if (reader.TokenType == JsonTokenType.PropertyName)
                 {
-                    var propertyName = reader.GetString();
-                    reader.Read();
-                    switch (propertyName?.ToLower())
+                    try
                     {
-                        case "first_name" when reader.TokenType == JsonTokenType.String:
-                            Console.WriteLine(reader.TokenType);
-                            fname = reader.GetString();
-                            break;
-                        case "second_name" when reader.TokenType == JsonTokenType.String:
-                            sname = reader.GetString();
-                            break;
-                        case "last_name" when reader.TokenType == JsonTokenType.String:
-                            lname = reader.GetString();
-                            break;
-                        case "birtdhday": 
-                            birthday = DateOnly.Parse(reader.GetString());
-                            break;
-                        case "phone_number" when reader.TokenType == JsonTokenType.String:
-                            phone_number = reader.GetString();
-                            break ;
+                        var propertyName = reader.GetString();
+                        reader.Read();
+                        switch (propertyName?.ToLower())
+                        {
+                            case "first_name" when reader.TokenType == JsonTokenType.String:
+                                Console.WriteLine(reader.TokenType);
+                                fname = reader.GetString();
+                                break;
+                            case "second_name" when reader.TokenType == JsonTokenType.String:
+                                sname = reader.GetString();
+                                break;
+                            case "last_name" when reader.TokenType == JsonTokenType.String:
+                                lname = reader.GetString();
+                                break;
+                            case "birtdhday":
+                                if (String.IsNullOrWhiteSpace(reader.GetString()))
+                                {
+                                    break;
+                                }
+                                birthday = DateOnly.Parse(reader.GetString());
+                                break;
+                            case "phone_number" when reader.TokenType == JsonTokenType.String:
+                                phone_number = reader.GetString();
+                                break;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("date Eroor");
                     }
                 }
             }
+            try
+            {
+                if (birthday == null || String.IsNullOrWhiteSpace(fname) || String.IsNullOrWhiteSpace(sname) || String.IsNullOrWhiteSpace(lname) || String.IsNullOrWhiteSpace(phone_number))
+                {
+                    // Add loger before
+
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ви ввели не всі данні!");
+            }
+            
+
             return new Persons() {first_name = fname,second_name = sname,last_name = lname,birthday = birthday , phone_number = phone_number};
         }
         
---
+++ Shop_control/Shop_control/Testing.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop_control
+{
+    public class Testing
+    {
+        bool TestForNullOrEmpty(string s)
+        {
+            bool result;
+            result = s == null || s == string.Empty;
+            return result;
+        }
+    }
+}
